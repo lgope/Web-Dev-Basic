d22caad252dd12566b6a86afd2cdb544a0516f98
lgope <ilakshmangope@gmail.com>
2020-02-05 00:55:09 +0600

    php crud lab practice.

---
+++ Web-Programming-SWE333A/php-crud/DBConfig.php
@@ -0,0 +1,27 @@
+<?php
+ 
+class DBConfig {
+    private $_host = "localhost";
+    private $_databaseUser = "root";
+    private $_password = "";
+    private $_dbname = "swe333";
+ 
+    protected $con;
+ 
+    public function __construct() {
+        if (empty($this->con)) {
+            $this->con = new mysqli($this->_host, $this->_databaseUser, $this->_password, $this->_dbname);
+ 
+            if ($this->con) {
+                return $this->con;
+            } else {
+                echo "Database not Connected!";
+                exit;
+            }
+        }
+ 
+        return $this->con;
+    }
+}
+ 
+?>
---
+++ Web-Programming-SWE333A/php-crud/add.php
@@ -0,0 +1,26 @@
+<?php
+include_once("Crud.php");
+ 
+$crud = new Crud();
+ 
+if (isset($_POST['addData'])) {
+    $name = $_POST['name'];
+    $student_id = $_POST['student_id'];
+    $email = $_POST['email'];
+ 
+$query = "INSERT INTO student_info(name, student_id, email) VALUES('$name', '$student_id', '$email')";
+ 
+if ($crud->execute($query)) {
+    echo "Insert Successfull!";
+} else {
+    echo "Insert Problem!";
+}
+}
+?>
+ 
+<form action="add.php" method="POST">
+<input type="text" name="name">
+<input type="text" name="student_id">
+<input type="email" name="email">
+<input type="submit" name="addData" value="Add Data To Server">
+</form>
